53dec7a57d392093762cf99bb43557797245c594
Ravimeena1 <rameena@bestpeers.com>
2020-01-24 11:56:35 +0530

    New solution.py for vivek

--- solution1.py
+++ solution1.py
@@ -9,13 +9,20 @@ A=[9,10 ,6, 8, 2 ,6, 1, 3 ,4]
 B=[1 ,2, 6, 4, 5 ,6 ,9 ,2, 6]
 list3=[]
 sum=0
+flag = True
 for i in range (len(A)-1):
-  if  A[i]>A[i+1]:
+   if flag==False:
+       flag=True
+       continue
+   if  A[i]>A[i+1]:
       sum+=B[i]
-  else:
+   else:
       if istrue(i,A):
 
-         list3.append((A[i]*(sum+B[i])))
+         list3.append((A[i]*(sum+B[i]+B[i+1])))
+         sum=0
+         flag=False
+         
       else:
 
           for j in range(i,len(A)):
